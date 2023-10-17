6583f0c9605eefe1db5f89ba6f50d205355e8892
Maloș Mihai <malosmihai1@yahoo.com>
2023-10-17 19:30:50 +0300

    java abandonat

---
+++ MIP/Laboratoare/Student/src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    int GroupId;
+    String firstName;
+    String lastName;
+    String cnp;
+    String email;
+
+    public Student(String firstName, String lastName, String cnp, String email)
+    {
+
+    }
+}
